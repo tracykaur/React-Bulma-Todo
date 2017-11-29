43df47478b1ed17ebe86d58605f53348b5c4fad9
tracykaur <tracykaur@yahoo.com>
2017-11-29 15:45:24 +1000

    challenge part 2 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Input, Title, SubTitle } from 'reactbulma'
+import { Input, Title, SubTitle, Notification } from 'reactbulma'
 import Header from './components/Header'
 
+let currentId = 5;
+const genId = () => ++currentId
 class App extends Component {
 
   state = {
     tasks: [
-      { todo:'Go for a run', time: '29/11/2017, 13:26:31', complete: true},
-      { todo:'Go for a walk', time: '29/11/2017, 13:26:31', complete: false}
+      { id : 1, todo:'Go for a run', time: '29/11/2017, 13:26:31', complete: true},
+      { id : 2, todo:'Go for a walk', time: '29/11/2017, 13:26:31', complete: false}
     ],
     searchPhrase: ''
   }
@@ -24,7 +26,7 @@ class App extends Component {
     const existingItem = this.state.tasks.find(task => task.todo === this.state.searchPhrase);
     if (!existingItem) {
       this.setState({
-        tasks: [{ todo: this.state.searchPhrase,
+        tasks: [{ id: genId(), todo: this.state.searchPhrase,
           time: new Date().toLocaleString()}, ...this.state.tasks ],
         // Reset search phrase to an empty string
         searchPhrase: ''
@@ -38,14 +40,23 @@ class App extends Component {
     // Update the state with the new Task
   }
 
+  toggleComplete = (id) => {
+    const foundTodoIndex = this.state.tasks.findIndex(task => task.id === id)
+    this.setState(prevState => {
+      const tasks = prevState.tasks
+      tasks[foundTodoIndex].complete = !tasks[foundTodoIndex].complete
+      return {tasks}
+    })
+  }
+
   render() {
 
     const  {tasks, searchPhrase } = this.state
     return (
       <div className='App'>
 
-        <Header totalIncomplete={ tasks.length } title='Tasks' />
-
+        <Header totalIncomplete={ tasks.filter(task => !task.complete).length } totalComplete={ tasks.filter(task => task.complete).length } />
+        <hr/>
         <form onSubmit={ this.addTask }>
            <Input primary large
            placeholder='search / add a todo!'
@@ -55,21 +66,18 @@ class App extends Component {
         </form>
         {
           tasks.filter(myTask => myTask.todo.includes(searchPhrase))
-          .map(myTask => <ListItem todo={myTask.todo}
-            time={myTask.time}
-            complete={myTask.complete} />)
+          .map(myTask => <ListItem {...myTask} toggleComplete={this.toggleComplete}/>)
         }
       </div>
     );
   }
 }
 
-const ListItem = ({ todo, time, complete }) => (
-  <div>
-    <Title is='3'>{todo}</Title>
+const ListItem = ({ todo, time, complete, toggleComplete, id }) => (
+  <Notification onClick={ () => toggleComplete(id)}>
+    {complete ? <Title is='3' className="completed">{todo}👻</Title> : <Title is="3">{todo}</Title>}
     <SubTitle is='6'>{time}</SubTitle>
-    {complete && <p>"👻"</p>}        
-  </div>
+  </Notification>
 )
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Progress, Level, Heading, Title } from 'reactbulma'
 
-const Header = ({ totalIncomplete, title }) => (
+const Header = ({ totalIncomplete, totalComplete  }) => (
   <div>
-    <Progress primary value="30" max="100">30%</Progress>
+    <Progress primary value={totalComplete/ (totalIncomplete+totalComplete) * 100} max="100"></Progress>
     <Level>
       <Level.Item hasTextCentered>
         <div>
-          <Heading>{ title }</Heading>
+          <Heading>INCOMPLETE</Heading>
           <Title>{ totalIncomplete }</Title>
         </div>
       </Level.Item>
+      <Level.Item hasTextCentered>
+        <div>
+          <Heading>COMPLETE</Heading>
+          <Title>{ totalComplete }</Title>
+        </div>
+      </Level.Item>
     </Level>
+
   </div>
 )
 
